c31743a954187db153783e693f960ebad3a396ce
Diego Alejandro <70347064+ecodiegoale@users.noreply.github.com>
2024-02-23 12:58:40 -0600

    Add files via upload

---
+++ trimestres_facets.R
@@ -0,0 +1,107 @@
+# Preparacion ========================================================
+setwd("G:/Mi unidad/r4me")
+rm(list = ls())
+library(siebanxicor)
+library(tidyverse)
+library(scales)
+library(ggtext)
+
+library(extrafont)
+font_import()
+y
+loadfonts(device = "win")
+
+## Token para usar siebanxicor ======================================
+token <- "464d59a88b68326e46c884bf1b1576b3b2a259b9983e5ed511b0d443fecc73b8"
+
+setToken(token)
+
+## Tema para el gráfico =============================================
+theme_da <- theme(panel.background = element_blank(),
+                  text = element_text(family="Dubai"),
+                    panel.border = element_blank(),
+                    legend.position = "none",
+                    axis.line = element_line(
+                      colour = "black", 
+                      linewidth= 0.8),
+                    axis.ticks = element_blank(),
+                    axis.text.y = element_text(
+                      colour = "black",
+                      size = 10),
+                    axis.text.x = element_text(
+                      colour = "black",
+                      size = 10),
+                    axis.title.y = element_blank(),
+                    axis.title.x = element_blank(),
+                    legend.key = element_blank(), #quita el fondo gris de las leyendas
+                    panel.grid.major.y =element_line(
+                      color = '#D7D6D6',
+                      linewidth = 0.8),
+                    panel.grid.minor.y = element_line(
+                      color = "#D7D6D6",
+                      linewidth = 0.1,
+                      linetype = 'dashed'),
+                    panel.grid.major.x = element_blank(),
+                    plot.title = element_text(
+                      size = 14, hjust = 0.5,
+                      face = "bold", colour = "black"),
+                    plot.caption = element_text(
+                      size = 8, hjust = 0.5,
+                      colour = "black"),
+                    plot.subtitle = ggtext::element_markdown(
+                      size = 12, hjust = 0.5,
+                      face = "bold", colour = "black"))
+
+# Datos del PIB =========================================================
+
+metadata <- getSeriesMetadata("SR17622")
+hoy <- Sys.Date() #mas reciente
+
+pib <- getSeriesData("SR17622", '2000-01-01', hoy)
+pib <- getSerieDataFrame(pib, "SR17622")
+
+## Tibble ===============================================================
+
+pib <- pib %>%
+filter(date > "2018-10-01")
+pib$year<- year(pib$date)
+pib$trim<- quarter(pib$date)
+
+pib <- pib %>%
+  mutate(
+    trim = as.character(trim),
+    trim = case_when(
+      trim == "1" ~ "I",
+      trim == "2" ~ "II",
+      trim == "3" ~ "III",
+      trim == "4" ~ "IV"
+    ),
+    value = value/1000
+  )
+
+pib_plot <- ggplot(data = pib, aes(x=trim, y=value))+
+  geom_bar(stat="identity",color ="#474F7A", fill="#474F7A")+
+  scale_y_continuous(label=comma)+
+  facet_wrap(~ year, nrow = 1,
+             strip.position = "bottom")+
+  theme_da+
+  facet_wrap(~ year, nrow = 1,
+             strip.position = "bottom")+
+  theme(strip.background = element_blank(),
+        strip.placement = "outside",
+        panel.spacing.x = unit(0, "cm"),
+        strip.text = element_text(colour = "black",
+                                  size = 10))+
+  labs(title = "PIB trimestral a precios constantes, base 2018",
+       subtitle =  "Miles de millones de pesos",
+       caption = "API, Banco de México | @ecodiegoale")
+pib_plot
+  
+ggsave("pib.png", plot = pib_plot, 
+       width = 20, height = 10, units = "cm")
+
+
+
+
+
+
